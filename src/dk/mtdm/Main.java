a2be7688ec9d57470f48f32c8bf7f61e5792db0e
Lukas <lukasvranic@gmail.com>
2022-03-30 00:07:01 +0200

    added GUI stuff

--- src/dk/mtdm/Main.java
+++ src/dk/mtdm/Main.java
@@ -1,11 +1,12 @@
 package dk.mtdm;
 import dk.mtdm.frontend.Draw;
 
+import java.awt.*;
+
 public class Main {
   public static void main(String[] args) {
-  Draw Draw = new Draw();
-  Draw.width = 1000;
-  Draw.dealer();
-  Draw.window();
+    final Dimension dim = Toolkit.getDefaultToolkit().getScreenSize(); //bruges til at lave en window til skærme stølsen
+    Draw Draw = new Draw();
+    Draw.initialize(dim.width, dim.height);
   }
 }
--- src/dk/mtdm/frontend/Draw.java
+++ src/dk/mtdm/frontend/Draw.java
@@ -1,23 +1,51 @@
 package dk.mtdm.frontend;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Draw {
-    public int width = 400;
-    public int height = 400;
-    private JFrame w = new JFrame();// creating a Frame
+    private final JFrame w = new JFrame();// creating a Frame
+    private final Dimension dim = Toolkit.getDefaultToolkit().getScreenSize(); //https://stackoverflow.com/questions/2442599/how-to-set-jframe-to-appear-centered-regardless-of-monitor-resolution
+        int width;
+        int height;
 
-    public void window() {
+    public void initialize(int w,int h){
+        width = w;
+        height = h;
+        button();
+        window();
+    }
+
+    private void window() {
+        w.setLayout(null);
         w.setTitle("Black Jack");
         w.setVisible(true);
         w.setSize(width ,height);
-        w.setLocation(200,200);
+        w.setLocation(dim.width/2-w.getSize().width/2, dim.height/2-w.getSize().height/2);
         w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//specifying close operation
     }
 
-    public void dealer() {
-        JLabel Dealer = new JLabel("Dealer");
+    private void button() {
+        JButton b = new JButton("Start");
+        b.setBounds(width/2-100/2 ,height/2-30/2,100,30);
+        b.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                b.setVisible(false);
+                dealer();
+            }
+        });
+        w.add(b);
+    }
+    private void dealer() {
+        JLabel Dealer = new JLabel("Dealer"); //der burde være en JTextField
+        ImageIcon icon = new ImageIcon("src/dk/mtdm/frontend/placeholder.png");
+        Dealer.setIcon(icon);
+        Dealer.setBounds(width/2-250/2 ,height/2-250/2, 250,250);
         w.add(Dealer);
+        w.revalidate();
+        w.repaint();
     }
 
 }
