0c71ad32100b94ba8cd85c8ae42bc3ff1e679adf
dendersen <dendersen@endersen.dk>
2022-03-29 16:07:12 +0200

    prep

--- src/dk/mtdm/backend/App.java
+++ src/dk/mtdm/backend/App.java
@@ -1,10 +1,7 @@
 package dk.mtdm.backend;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-    public void go(){
-        
-    }
+  public static void main() throws Exception {
+    System.out.println("Hello, World!");
+  }
 }
