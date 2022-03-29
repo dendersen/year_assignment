1b07f5cc7c1c0a7a8b0000e9413a9efc067bd219
Lukas <lukasvranic@gmail.com>
2022-03-29 22:31:06 +0200

    added stuff

--- src/dk/mtdm/frontend/Draw.java
+++ src/dk/mtdm/frontend/Draw.java
@@ -1,5 +1,23 @@
 package dk.mtdm.frontend;
 
+import javax.swing.*;
+
 public class Draw {
-  
+    public int width = 400;
+    public int height = 400;
+    private JFrame w = new JFrame();// creating a Frame
+
+    public void window() {
+        w.setTitle("Black Jack");
+        w.setVisible(true);
+        w.setSize(width ,height);
+        w.setLocation(200,200);
+        w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//specifying close operation
+    }
+
+    public void dealer() {
+        JLabel Dealer = new JLabel("Dealer");
+        w.add(Dealer);
+    }
+
 }
