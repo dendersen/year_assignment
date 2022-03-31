07cd272c6181435585ce212f0f16764c468cf5e8
dendersen <dendersen@endersen.dk>
2022-03-31 22:43:26 +0200

    adding saveing

--- src/dk/mtdm/backend/BlackJack/PlayerHandObject.java
+++ src/dk/mtdm/backend/BlackJack/PlayerHandObject.java
@@ -3,6 +3,9 @@ package dk.mtdm.backend.BlackJack;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.io.FileWriter;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
 
 public class PlayerHandObject {
   private static ArrayList<CardObject> hand = new ArrayList<CardObject>();
@@ -13,9 +16,9 @@ public class PlayerHandObject {
   public static ArrayList<CardObject> getHand(){
     return(hand);
   }
-  public static void main(){
+  public static void save(byte handId){
     try{
-      File myObj = new File("filename.txt");
+      File myObj = new File("BlackJack save game.txt" + handId);
       if(myObj.createNewFile()){
         System.out.println("file created: " + myObj.getName());
       }else {
@@ -25,5 +28,40 @@ public class PlayerHandObject {
       System.out.println("an error occured while making file");
       e.printStackTrace();
     }
+
+    try {
+      FileWriter myWriter = new FileWriter("BlackJack save game.txt");
+      myWriter.write("Files in Java might be tricky, but it is fun enough!");
+      myWriter.close();
+      System.out.println("Successfully wrote to the file.");
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
+  public static void load(){
+    String data = "";
+    try {
+      File myObj = new File("BlackJack save game.txt");
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        data += myReader.nextLine();
+        data += "\n";
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+    hand = decode(data);
+  }
+  
+  private static ArrayList<CardObject> decode(String data){
+    ArrayList<CardObject> hand = new ArrayList<CardObject>();
+    String[] dataSplit = data.split("{");
+    for(byte i = 0; i < dataSplit.length; i++){
+      dataSplit[1].replace("}", "");
+    }
+    return(hand);
   }
 }
