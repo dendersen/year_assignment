22c357262a1f85602b5bb932907b76a147fca3ad
dendersen <dendersen@endersen.dk>
2022-03-31 21:02:48 +0200

    adding sve feature

--- src/dk/mtdm/backend/BlackJack/PlayerHandObject.java
+++ src/dk/mtdm/backend/BlackJack/PlayerHandObject.java
@@ -1,11 +1,29 @@
 package dk.mtdm.backend.BlackJack;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class PlayerHandObject {
-  private ArrayList<CardObject> hand = new ArrayList<>();
+  private static ArrayList<CardObject> hand = new ArrayList<CardObject>();
   
-  public PlayerHandObject(CardObject hand){
-    this.hand.add(hand);
+  public static void addCard(CardObject card){
+    hand.add(card);
+  }
+  public static ArrayList<CardObject> getHand(){
+    return(hand);
+  }
+  public static void main(){
+    try{
+      File myObj = new File("filename.txt");
+      if(myObj.createNewFile()){
+        System.out.println("file created: " + myObj.getName());
+      }else {
+        System.out.println("file already exists.");
+      }
+    }catch (IOException e){
+      System.out.println("an error occured while making file");
+      e.printStackTrace();
+    }
   }
 }
