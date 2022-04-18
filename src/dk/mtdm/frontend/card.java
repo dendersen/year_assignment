d550eb5f4aedd918199106f45be338639c5ede3e
dendersen <dendersen@endersen.dk>
2022-04-18 21:59:03 +0200

    ¨simplified code

--- src/dk/mtdm/frontend/card.java
+++ src/dk/mtdm/frontend/card.java
@@ -25,87 +25,82 @@ public class card implements ImageObserver {
     }
 
     public void show(Graphics g) {
+        String path = "src/dk/mtdm/frontend/icons/";
         if (this.dealer){
-            this.y += 25;
-            if (this.id == 0) {
-                g.setColor(Color.red);
-            } else {
-                g.setColor(Color.white);
-            }
-            if (this.id == 0) { //Checker om det er dealens første kort
-                Image back = new ImageIcon("src/dk/mtdm/frontend/icons/back.png").getImage();
-                g.drawImage(back,
-                        this.x() + width/2-this.w(), this.y(),
-                        this);
-            } else {
-                g.fillRect(this.x() + width / 2 - this.w(), this.y(), this.w(), this.h());
-                g.setColor(Color.black);
-                g.drawRect(this.x() + width / 2 - this.w(), this.y(), this.w(), this.h());
+            dealerCode(g, path);
+        } else {
+            playerCode(g, path);
+        }
+    }
 
-                switch (symbol) {
-                    case "hjerter":
-                        Image hjerter = new ImageIcon("src/dk/mtdm/frontend/icons/hearts.png").getImage();
-                        g.drawImage(hjerter,
-                                this.x() + this.w() / 2 - 64 + width / 2 - this.w(),
-                                this.y() + this.h() / 2,
-                                this);
-                    case "romber":
-                        Image romber = new ImageIcon("src/dk/mtdm/frontend/icons/hearts.png").getImage();
-                        g.drawImage(romber,
-                                this.x() + this.w() / 2 - 64 + width / 2 - this.w(),
-                                this.y() + this.h() / 2,
-                                this);
-                    case "klør":
-                        Image klor = new ImageIcon("src/dk/mtdm/frontend/icons/hearts.png").getImage();
-                        g.drawImage(klor,
-                                this.x() + this.w() / 2 - 64 + width / 2 - this.w(),
-                                this.y() + this.h() / 2,
-                                this);
-                    case "spar":
-                        Image spar = new ImageIcon("src/dk/mtdm/frontend/icons/hearts.png").getImage();
-                        g.drawImage(spar,
-                                this.x() + this.w() / 2 - 64 + width / 2 - this.w(),
-                                this.y() + this.h() / 2,
-                                this);
-                }
-                g.setFont(new Font("consolas", Font.BOLD, 128));
-                g.drawString(this.number, this.x() + this.w() / 2 - 36 + width / 2 - this.w(), this.y() + this.h() / 2 - 32);
+    private void playerCode(Graphics g, String path) {
+        int pathY = this.y()+height-this.h()-50;
+        int pathX = this.x() + width/2-this.w();
+        g.setColor(Color.white);
+        g.fillRect(pathX, pathY, this.w(), this.h());
+        g.setColor(Color.black);
+        g.drawRect(pathX, pathY, this.w(), this.h());
+        pathX = this.x() + this.w() / 2-64 + width/2-this.w();
+        pathY = this.y()+height-this.h()/2-30;
+        switch (symbol){
+            case "hjerter":
+                Image hjerter = new ImageIcon(path + "hearts.png").getImage();
+                g.drawImage(hjerter, pathX, pathY, this);
+            case "romber":
+                Image romber = new ImageIcon(path + "hearts.png").getImage();
+                g.drawImage(romber, pathX, pathY, this);
+            case "klør":
+                Image klor = new ImageIcon(path + "hearts.png").getImage();
+                g.drawImage(klor, pathX, pathY, this);
+            case "spar":
+                Image spar = new ImageIcon(path + "hearts.png").getImage();
+                g.drawImage(spar, pathX, pathY, this);
+        }
+        g.setFont(new Font("consolas",Font.BOLD,130));
+        g.drawString(this.number,this.x()+width/2-this.w()/2-36,this.y()+height/2+this.h()/2+128);
+    }
 
-            }
+    private void dealerCode(Graphics g, String path) {
+        this.y += 25;
+        if (this.id == 0) {
+            g.setColor(Color.red);
         } else {
             g.setColor(Color.white);
-            g.fillRect(this.x() + width/2-this.w(), this.y()+height-this.h()-50, this.w(), this.h());
-            g.setColor(Color.black);
-            g.drawRect(this.x() + width/2-this.w(), this.y()+height-this.h()-50, this.w(), this.h());
-            switch (symbol){
-                case "hjerter":
-                    Image hjerter = new ImageIcon("src/dk/mtdm/frontend/icons/hearts.png").getImage();
-                    g.drawImage(hjerter,
-                            this.x() + this.w() / 2-64 + width/2-this.w(),
-                            this.y()+height-this.h()/2-30,
-                            this);
-                case "romber":
-                    Image romber = new ImageIcon("src/dk/mtdm/frontend/icons/hearts.png").getImage();
-                    g.drawImage(romber,
-                            this.x() + this.w() / 2-64 + width/2-this.w(),
-                            this.y()+height-this.h()/2-30,
-                            this);
-                case "klør":
-                    Image klor = new ImageIcon("src/dk/mtdm/frontend/icons/hearts.png").getImage();
-                    g.drawImage(klor,
-                            this.x() + this.w() / 2-64 + width/2-this.w(),
-                            this.y()+height-this.h()/2-30,
-                            this);
-                case "spar":
-                    Image spar = new ImageIcon("src/dk/mtdm/frontend/icons/hearts.png").getImage();
-                    g.drawImage(spar,
-                            this.x() + this.w() / 2-64 + width/2-this.w(),
-                            this.y()+height-this.h()/2-30,
-                            this);
-            }
-            g.setFont(new Font("consolas",Font.BOLD,130));
-            g.drawString(this.number,this.x()+width/2-this.w()/2-36,this.y()+height/2+this.h()/2+128);
         }
+        if (this.id == 0) { //Checker om det er dealens første kort
+            Image back = new ImageIcon("src/dk/mtdm/frontend/icons/back.png").getImage();
+            g.drawImage(back,
+                    this.x() + width/2-this.w(), this.y(),
+                    this);
+        } else {
+            DrawCard(g, path);
+
+        }
+    }
+
+    private void DrawCard(Graphics g, String path) {
+        g.fillRect(this.x() + width / 2 - this.w(), this.y(), this.w(), this.h());
+        g.setColor(Color.black);
+        g.drawRect(this.x() + width / 2 - this.w(), this.y(), this.w(), this.h());
+
+        int simpX = this.x() + this.w() / 2 - 64 + width / 2 - this.w();
+        int simpY = this.y() + this.h() / 2;
+        switch (symbol) {
+            case "hjerter":
+                Image hjerter = new ImageIcon(path + "hearts.png").getImage();
+                g.drawImage(hjerter, simpX,simpY, this);
+            case "romber":
+                Image romber = new ImageIcon(path + "hearts.png").getImage();
+                g.drawImage(romber, simpX,simpY, this);
+            case "klør":
+                Image klor = new ImageIcon(path + "hearts.png").getImage();
+                g.drawImage(klor, simpX,simpY, this);
+            case "spar":
+                Image spar = new ImageIcon(path + "hearts.png").getImage();
+                g.drawImage(spar, simpX,simpY, this);
+        }
+        g.setFont(new Font("consolas", Font.BOLD, 128));
+        g.drawString(this.number, this.x() + this.w() / 2 - 36 + width / 2 - this.w(), this.y() + this.h() / 2 - 32);
     }
 
     public int x() {
