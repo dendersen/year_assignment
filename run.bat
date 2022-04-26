fd45cc7c46582b93275316129dbd6d8fd30aa399
dendersen <dendersen@endersen.dk>
2022-04-26 18:08:04 +0200

    more compile stuff

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
-javac @compile.txt src\dk\mtdm\Main.java
-cd bin
-cmd ""C:\Program Files\Java\jdk-17.0.2\bin.java"" Main
-pause
-cd..
+cd .\src
+javac @..\compile.txt dk\mtdm\Main.java
+cd .\..\bin
+cmd /c ""c:/program files/java/jdk-17/bin/java"" Main
+cd .\..
+echo "program finished"
--- src/dk/mtdm/Main.java
+++ src/dk/mtdm/Main.java
@@ -7,6 +7,6 @@ public class Main {
   public static void main(String[] args) {
     Draw window = new Draw();
     Table table = new Table((byte) (3), (byte) (1));
-    table.setup();
+    table.TableSetup();
   }
 }
