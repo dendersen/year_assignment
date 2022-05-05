e7e678cd9a12cfdeba0219a966a71587927df2f5
dendersen <dendersen@endersen.dk>
2022-05-05 09:06:49 +0200

    clean up

---
+++ src/dk/mtdm/controller/CurrentData.java
@@ -0,0 +1,23 @@
+package dk.mtdm.controller;
+
+import dk.mtdm.backend.BlackJack.Table;
+
+public class CurrentData {
+  public byte playerID;
+  public boolean[] AVAILABLE_ACTIONS;
+  public boolean action;
+
+
+  public CurrentData(byte PlayerID) {
+    playerID = PlayerID;
+    AVAILABLE_ACTIONS = Table.availableActions(playerID);
+  }
+
+  /**
+   * 
+   * @param Action true = hit, false = stand
+   */
+  public void setAction(boolean Action){
+    action = Action;
+  }
+}
