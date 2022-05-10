9e9a5230b67d27561e99e86099fd87f919215c54
dendersen <dendersen@endersen.dk>
2022-05-10 17:39:58 +0200

    compatibility adding

--- src/dk/mtdm/frontend/Draw.java
+++ src/dk/mtdm/frontend/Draw.java
@@ -9,8 +9,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.ImageObserver;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import static dk.mtdm.frontend.Draw.MyCanvas.g;
 
