a40f56f13ba889810b3ae8126f2638d029f397ff
dendersen <dendersen@endersen.dk>
2022-05-17 01:41:47 +0200

    made a lot of final variables

--- src/dk/mtdm/backend/BlackJack/BlackJackProcessing.java
+++ src/dk/mtdm/backend/BlackJack/BlackJackProcessing.java
@@ -1,24 +1,44 @@
 package dk.mtdm.backend.BlackJack;
 
 public class BlackJackProcessing {
-  public static void hit (byte playerID,boolean debug){
+  final static byte MAX_DEATH_VALUE = 21;
+  final static byte ES_VALUE_SMALL = 1;
+  final static byte ES_VALUE_BIG = 11;
+  final static byte ES_CARD_NUMBER = 1;
+  final static byte STANDARD_BIG_CARD_VALUE = 10;
+  
+  final static byte highestValue () {
+    if(ES_VALUE_SMALL > STANDARD_BIG_CARD_VALUE){
+      return ES_VALUE_BIG;
+    }
+    return STANDARD_BIG_CARD_VALUE;
+  }
+
+  public static byte[] getFinals(){
+    return new byte[] {MAX_DEATH_VALUE, ES_VALUE_SMALL, ES_VALUE_BIG, ES_CARD_NUMBER, STANDARD_BIG_CARD_VALUE};
+  }
+
+
+  public static void hit (byte playerID){
     Table.getPlayer(playerID).addCard(Table.drawCards((byte) (1))[0]);
-    if(debug)
-    System.out.println(Table.getPlayer(playerID).getHand().get(Table.getPlayer(playerID).getHand().size()-1).getNumber());
   }
   public static byte playerValue(byte playerID){
     byte total = 0;
     byte es = 0;
     for(byte index = 0; index < Table.getPlayer(playerID).getHand().size(); index++){
-      if(Table.getPlayer(playerID).getHand().get(index).getNumber() == 1){
-      total += 11;
+      byte currentNumber = Table.getPlayer(playerID).getHand().get(index).getNumber();
+      if(currentNumber == ES_CARD_NUMBER){
+      total += ES_VALUE_BIG;
       es++;
       }
-      else total += Table.getPlayer(playerID).getHand().get(index).getNumber();
+      else if(currentNumber <= STANDARD_BIG_CARD_VALUE)
+        total += currentNumber;
+      else 
+        total += 10;
     }
-    while (total >= 21){
+    while (total >= MAX_DEATH_VALUE){
       if (es > 0){
-      total -= 10;
+      total -= ES_VALUE_BIG - ES_VALUE_SMALL;
       es--;
     }else
       break;
@@ -29,7 +49,7 @@ public class BlackJackProcessing {
   public static byte hasEs(byte playerID){
     byte es = 0;
     for(byte index = 0; index < Table.getPlayer(playerID).getHand().size(); index++){
-      if(Table.getPlayer(playerID).getHand().get(index).getNumber() == 1){
+      if(Table.getPlayer(playerID).getHand().get(index).getNumber() == ES_CARD_NUMBER){
       es++;
       }
     }
@@ -37,7 +57,7 @@ public class BlackJackProcessing {
   }
   
   public static boolean isAlive(byte playerID){
-    return(playerValue(playerID)<=21);
+    return(playerValue(playerID)<=MAX_DEATH_VALUE);
   }
   
   /**
@@ -47,7 +67,7 @@ public class BlackJackProcessing {
     byte[] scores = new byte[Table.NUMBER_OF_PLAYERS];
     for(byte i = 0; i < Table.NUMBER_OF_PLAYERS; i++){
       scores[i] = playerValue(i);
-      if(scores[i]>21)
+      if(scores[i]>MAX_DEATH_VALUE)
       scores[i] = -1;
     }
     byte largestIndex = 0;
@@ -61,7 +81,7 @@ public class BlackJackProcessing {
   public static boolean[] availablePlayerActions (byte playerValue){
     boolean[] actions = new boolean[3];
     
-    if(playerValue > 21){
+    if(playerValue > MAX_DEATH_VALUE){
       actions[0] = false;
       actions[1] = false;
       actions[2] = false;
@@ -71,7 +91,7 @@ public class BlackJackProcessing {
       actions[1] = true;
       actions[2] = false;
     }
-    if(playerValue <= 10){
+    if(playerValue <= highestValue()){
       actions[2] = true;
     }
     return (actions);
